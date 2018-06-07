6d372b7c08e03c2f5d528f68f73e9395f4b4680c
Rick Jelier <rick@hollandshielding.com>
2018-06-07 20:35:07 +0200

    Validation rules

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,10 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Appointment;
 use Auth;
+use Validator;
 
 class AppointmentController extends Controller 
 {
 
+    /**
+     * Get a validator for an incoming post request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return $validator = Validator::make($data, array(
+            'title' => 'required|string|max:255',
+            'postal' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'start_date' => 'required|date_format:d-m-Y|max:255',
+            'start_time' => 'string|max:255',
+            'end_date' => 'string|date_format:d-m-Y|max:255',
+            'end_time' => 'string|date_format:G:i|max:255',
+            'kilometers' => 'string|max:255',
+            'description' => 'string|max:255',
+        ));
+    }
+    
     public function index() {
         $user = Auth::user();
         return view('index', compact('user'));
@@ -19,6 +42,18 @@ class AppointmentController extends Controller
     }
 
     public function create(Request $request) {
+        $request->validate(array(
+            'title' => 'required|alpha_num|max:255',
+            'postal' => 'required|regex:/^\d\d\d\d\s?[A-z][A-z]$/',
+            'address' => 'required|alpha_num|max:255',
+            'city' => 'required|alpha|max:255',
+            'start_date' => 'required|date_format:j-n-Y|max:255',
+            'start_time' => 'string|date_format:G:i|max:255',
+            'end_date' => 'date_format:j-n-Y|max:255',
+            'end_time' => 'date_format:G:i|max:255',
+            'kilometers' => 'integer|max:255',
+        ));
+        
         $appointment = new Appointment();
         $appointment->title = $request->title;
         $appointment->postal = $request->postal;
@@ -50,6 +85,18 @@ class AppointmentController extends Controller
             $appointment->delete();
             return redirect('/');
         } else {
+            $request->validate(array(
+                'title' => 'required|alpha_num|max:255',
+                'postal' => 'required|regex:/^\d\d\d\d\s?[A-z][A-z]$/',
+                'address' => 'required|alpha_num|max:255',
+                'city' => 'required|alpha|max:255',
+                'start_date' => 'required|date_format:j-n-Y|max:255',
+                'start_time' => 'string|date_format:G:i|max:255',
+                'end_date' => 'date_format:j-n-Y|max:255',
+                'end_time' => 'date_format:G:i|max:255',
+                'kilometers' => 'integer|max:255',
+            ));
+            
             $appointment->title = $request->title;
             $appointment->postal = $request->postal;
             $appointment->address = $request->address;
